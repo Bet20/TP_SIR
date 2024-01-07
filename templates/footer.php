5ece2b31c72ad3c013395194391ad06676b5a072
Bet20 <55533920+Bet20@users.noreply.github.com>
2024-01-07 00:23:52 +0000

    fixed the footer being not sticked to the bottom and improved the UI

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,6 @@
-    <footer>
-      <hr style="border: 1px solid black;opacity: 1">
-      <div class="text-center mb-3">
+    <footer class="footer fixed-bottom bg-white">
+      <hr class="m-0" style="border: 1px solid black;opacity: 1">
+      <div class="text-center mt-3 mb-3">
         <span>&copy; Copyright 2023</span><br>
         <span>Made by <b>Diogo Carvalho</b> & <b>João Freitas</b></span>
       </div>
